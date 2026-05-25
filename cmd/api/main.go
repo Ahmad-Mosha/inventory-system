8247e11e9e4fb8008b6b64e5fd50091b193a5e01
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2026-05-25 16:54:59 +0300

    basic config for gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,19 @@
-package api
+package main
 
 import (
 	"log"
 
 	"github.com/Ahmad-Mosha/inventory-system/internal/repository"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db , err := repository.InitDB("inventory.db")
+	db, err := repository.InitDB("inventory.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	
 
+	router := gin.Default()
+	router.Run()
 }
