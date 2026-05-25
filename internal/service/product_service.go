f75a990021daaa3cc5491209bfc62a2fc0d99922
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2026-05-25 16:17:30 +0300

    install gin and start http handling

--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/Ahmad-Mosha/inventory-system/internal/models"
 	"github.com/Ahmad-Mosha/inventory-system/internal/repository"
 )
@@ -18,5 +20,29 @@ func NewProductService(repo repository.ProductRepository) ProductService {
 	return &productService{repo: repo}
 }
 
-func (s *productService) RegisterProduct(p *models.Product) error
-func (s *productService) SellProduct(id, quantity int) error
+func (s *productService) RegisterProduct(p *models.Product) error {
+	if p.Price < 0 {
+		return fmt.Errorf("Product price must be greater than zero")
+	}
+
+	if p.StockQuantity < 0 {
+		return fmt.Errorf("business error: initial stock cannot be negative")
+	}
+	if p.Name == "" {
+		return fmt.Errorf("business error: product name cannot be empty")
+	}
+
+	return s.repo.Create(p)
+}
+
+func (s *productService) SellProduct(id, quantity int) error {
+	p, err := s.repo.GetByID(id)
+	if err != nil {
+		return err
+	}
+	if p.StockQuantity < quantity {
+		return fmt.Errorf("Quantity is not enough")
+	}
+	p.StockQuantity -= quantity
+	return s.repo.Update(id, p)
+}
