02870bbdeb78b3ff88b7cc5bfaf80ca65b49df2f
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2026-05-26 15:47:22 +0300

    add json tags

--- internal/handler/product_handler.go
+++ internal/handler/product_handler.go
@@ -1,3 +1,18 @@
 package handler
 
+import (
+	"github.com/Ahmad-Mosha/inventory-system/internal/service"
+	"github.com/gin-gonic/gin"
+)
 
+type ProductHandler struct {
+	srv service.ProductService
+}
+
+func NewProductHandler(srv service.ProductService) *ProductHandler {
+	return &ProductHandler{srv: srv}
+}
+
+func (h *ProductHandler) GetProduct(c *gin.Context) {
+	
+}
--- internal/models/models.go
+++ internal/models/models.go
@@ -3,23 +3,23 @@ package models
 import "time"
 
 type Product struct {
-    ID            int     `db:"id"`
-    Name          string  `db:"name"`
-    Price         float64  `db:"price"`
-    StockQuantity int     `db:"stock_quantity"`
+	ID            int     `db:"id" json:"id"`
+	Name          string  `db:"name" json:"name"`
+	Price         float64 `db:"price" json:"price"`
+	StockQuantity int     `db:"stock_quantity" json:"stock_quantity"`
 }
 
 type Customer struct {
-    ID    int    `db:"id"`
-    Name  string `db:"name"`
-    Email string `db:"email"`
+	ID    int    `db:"id"`
+	Name  string `db:"name"`
+	Email string `db:"email"`
 }
 
 type Order struct {
-    ID         int       `db:"id"`
-    CustomerID int       `db:"customer_id"`
-    ProductID  int       `db:"product_id"`
-    Quantity   int       `db:"quantity"`
-    Status     string    `db:"status"`
-    CreatedAt  time.Time `db:"created_at"`
+	ID         int       `db:"id"`
+	CustomerID int       `db:"customer_id"`
+	ProductID  int       `db:"product_id"`
+	Quantity   int       `db:"quantity"`
+	Status     string    `db:"status"`
+	CreatedAt  time.Time `db:"created_at"`
 }
